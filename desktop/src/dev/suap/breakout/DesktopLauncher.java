0e7a3e9616677706970cbd86633c2d3bbe2a29f5
suap-dev <suap.dev@gmail.com>
2023-02-02 00:21:47 +0100

    first commit

---
+++ desktop/src/dev/suap/breakout/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package dev.suap.breakout;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import dev.suap.breakout.BreakoutGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Suap's Breakout");
+		new Lwjgl3Application(new BreakoutGame(), config);
+	}
+}
