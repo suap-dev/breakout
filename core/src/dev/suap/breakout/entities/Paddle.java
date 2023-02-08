511c750864a1908a79918ce83a248c9fc0653d38
suap-dev <suap.dev@gmail.com>
2023-02-08 03:51:06 +0100

    trap the paddle in the screen

--- core/src/dev/suap/breakout/entities/Paddle.java
+++ core/src/dev/suap/breakout/entities/Paddle.java
@@ -10,21 +10,26 @@ public class Paddle extends RectangularEntity {
     private Ball ball;
     private Vector2 ballLaunchVelocity;
     private boolean hasBall = false;
+    private final float screenWidth;
 
     public Paddle(Vector2 origin, float width, float height, float rotation) {
         super(origin, width, height, rotation);
+        screenWidth = Gdx.graphics.getWidth();
     }
 
     public Paddle(Vector2 origin, float width, float height) {
         super(origin, width, height);
+        screenWidth = Gdx.graphics.getWidth();
     }
 
     public Paddle(float x, float y, float width, float height, float rotation) {
         super(x, y, width, height, rotation);
+        screenWidth = Gdx.graphics.getWidth();
     }
 
     public Paddle(float x, float y, float width, float height) {
         super(x, y, width, height);
+        screenWidth = Gdx.graphics.getWidth();
     }
 
     public Ball spawnBall(float radius, float velocity) {
@@ -43,9 +48,16 @@ public class Paddle extends RectangularEntity {
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        this.setOriginX(Gdx.input.getX());
+
+        setOriginX(Gdx.input.getX());
+
+        if (getLeft() < 1)
+            setOriginX(1 + getWidth() / 2);
+        else if (this.getRight() > screenWidth - 1)
+            setOriginX(screenWidth - 1 - getWidth() / 2);
+
         if (hasBall) {
-            this.ball.setOriginX(Gdx.input.getX());
+            this.ball.setOriginX(this.getOriginX());
             if (Gdx.input.isButtonPressed(Buttons.LEFT)) {
                 ball.setVelocity(ballLaunchVelocity);
                 hasBall = false;
